5b39987579d09468c600fe3d1f24f5e2876e7ff7
barrystone <barrystone3183@gmail.com>
2023-01-01 16:00:11 +0800

    [add] infos data update

--- data/infos.ts
+++ data/infos.ts
@@ -1,7 +1,24 @@
 // const infos: Array<Object> = [
 const infos: Array<{ [name: string]: string }> = [
-  { title: 'asdt', desc: 'ada', image: '/assets/taoyuan-tourism-bg-2.jpeg' },
-  { title: 'tasd', desc: 'awda', image: '/assets/taoyuan-tourism-bg-2.jpeg' }
+  {
+    title: 'Taoyuan Public Library',
+    desc: 'Taoyuan Public Library is designed with the inspiration of "Tree of Life". It has 8 floors and a collection of nearly one million books.',
+    image: '/assets/taoyuan-tourism-info-3.jpeg',
+    more: 'https://travel.tycg.gov.tw/zh-tw/travel/attraction/1656'
+  },
+  {
+    title: 'Shimen Dam',
+    desc: 'Throughout the year, the lakes and mountains are beautiful and the scenery is beautiful. There are bicycle trails and yachts around the lake.',
+    image: '/assets/taoyuan-tourism-info-2.jpeg',
+    more: 'https://travel.tycg.gov.tw/zh-tw/travel/attraction/351'
+  },
+
+  {
+    title: 'Youling Waterfall',
+    desc: 'Visit the most spectacular ghost waterfall in Lala Mountain, and the white silk-like Shuiliandong Waterfall.',
+    image: '/assets/taoyuan-tourism-info-1.jpeg',
+    more: 'https://travel.tycg.gov.tw/zh-tw/travel/attraction/1469'
+  }
 ];
 
 export default infos;
